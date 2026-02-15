38c59ab11d07e845124c0cb297deca9d48e7398b
narkaTee <narkaTee@users.noreply.github.com>
2026-02-15 19:56:47 +0100

    Install brave on workstation

--- modules/workstation-desktop.nix
+++ modules/workstation-desktop.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    chromium
+  ];
 
   services.xrdp.enable = true;
   services.xrdp.openFirewall = false;
